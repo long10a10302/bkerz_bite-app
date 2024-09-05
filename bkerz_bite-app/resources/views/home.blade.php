a50b1ffe65f0a1fd915ae3acba3efccff7a96e67
Valiant <rainguy.shy@gmail.com>
2024-09-05 23:05:37 +0700

    add home slider
    chạy lại npm install trước project

--- bkerz_bite-app/resources/views/home.blade.php
+++ bkerz_bite-app/resources/views/home.blade.php
@@ -1,17 +1,82 @@
-
 <!doctype html>
 <html>
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+    {{-- <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css" rel="stylesheet" /> --}}
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
 </head>
+
 <body>
-  @include('layouts.header')
-  
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  @extends('layouts.footer2')
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
+
+    @include('layouts.header')
+    <div>
+
+        <div id="gallery" class="relative w-screen" data-carousel="slide" data-carousel-interval="3000">
+            <!-- Carousel wrapper -->
+            <!-- Increase height here -->
+            <div class="relative h-[400px] md:h-[600px] overflow-hidden bg-[#EBE1CB]">
+                <!-- Item 1 -->
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="/images/Bread-slide-1853.jpg" class="absolute inset-0 w-full h-full object-cover"
+                        alt="Bread slide">
+                </div>
+                <!-- Item 2 -->
+                <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+                    <img src="/images/caramello-slice-e1632865980223.jpg"
+                        class="absolute inset-0 w-full h-full object-cover" alt="Caramello slice">
+                </div>
+                <!-- Item 3 -->
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="/images/croissants.jpg" class="absolute inset-0 w-full h-full object-cover"
+                        alt="Croissants">
+                </div>
+                <!-- Item 4 -->
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="/images/gallery_template_cookies_3.jpg"
+                        class="absolute inset-0 w-full h-full object-cover" alt="Cookies">
+                </div>
+                <!-- Item 5 -->
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="/images/pastry-0491.jpg" class="absolute inset-0 w-full h-full object-cover"
+                        alt="Pastry">
+                </div>
+            </div>
+
+            <!-- Slider controls -->
+            <button type="button"
+                class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                data-carousel-prev>
+                <span
+                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M5 1 1 5l4 4" />
+                    </svg>
+                    <span class="sr-only">Previous</span>
+                </span>
+            </button>
+            <button type="button"
+                class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                data-carousel-next>
+                <span
+                    class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 9 4-4-4-4" />
+                    </svg>
+                    <span class="sr-only">Next</span>
+                </span>
+            </button>
+        </div>
+
+    </div>
+    @extends('layouts.footer2')
 </body>
+
 </html>
